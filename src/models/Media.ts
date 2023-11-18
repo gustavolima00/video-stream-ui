758605c76ea124a1fa124a00303f93bf59e5fa54
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-11-18 13:43:50 -0300

    Edition of home page

---
+++ src/models/Media.ts
@@ -0,0 +1,84 @@
+export interface Media {
+  id: number;
+  title: string;
+  description: string;
+  kind: string;
+  image?: string;
+}
+
+export const sampleMediaList: Media[] = [
+  {
+    id: 1,
+    title: "The Big Bang Theory",
+    description: "Season 1",
+    kind: "series",
+  },
+  {
+    id: 2,
+    title: "The Big Bang Theory",
+    description: "Season 2",
+    image: "https://i.imgur.com/3n9xOxt.jpg",
+    kind: "series",
+  },
+  {
+    id: 3,
+    title: "The Big Bang Theory",
+    description: "Season 3",
+    image: "https://i.imgur.com/3n9xOxt.jpg",
+    kind: "series",
+  },
+  {
+    id: 4,
+    title: "The Big Bang Theory",
+    description: "Season 4",
+    kind: "series",
+  },
+  {
+    id: 5,
+    title: "The Big Bang Theory",
+    description: "Season 5",
+    image: "https://i.imgur.com/3n9xOxt.jpg",
+    kind: "series",
+  },
+  {
+    id: 6,
+    title: "The Big Bang Theory",
+    description: "Season 6",
+    image: "https://i.imgur.com/3n9xOxt.jpg",
+    kind: "series",
+  },
+  {
+    id: 7,
+    title: "The Big Bang Theory",
+    description: "Season 7",
+    image: "https://i.imgur.com/3n9xOxt.jpg",
+    kind: "series",
+  },
+  {
+    id: 8,
+    title: "The Big Bang Theory",
+    description: "Season 8",
+    kind: "series",
+  },
+  {
+    id: 9,
+    title: "The Big Bang Theory",
+    description: "Season 9",
+    image: "https://i.imgur.com/3n9xOxt.jpg",
+    kind: "series",
+  },
+  {
+    id: 10,
+    title: "The Big Bang Theory",
+    description: "Season 10",
+    image: "https://i.imgur.com/3n9xOxt.jpg",
+    kind: "series",
+  },
+  {
+    id: 11,
+    title: "A bela e a fera",
+    description: "",
+    image: "https://i.imgur.com/3n9xOxt.jpg",
+    kind: "movie",
+  },
+];
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,117 +1,71 @@
 import { useEffect, useState } from "react";
-import { Card, Button, Form, Container, Row, Col } from "react-bootstrap";
+import { Card, Button, Form, Container, Row, Col, Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { sampleMediaList, Media } from "../models/Media";
 
 function HomePage() {
-  const [medias, setMedias] = useState<any[]>([]);
+  const [medias, setMedias] = useState<Media[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
+
+  const fetchMedias = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return sampleMediaList;
+  };
+
   useEffect(() => {
-    setMedias([
-      {
-        id: 1,
-        title: "The Big Bang Theory",
-        description: "Season 1",
-        image: "https://i.imgur.com/3n9xOxt.jpg",
-        kind: "series",
-      },
-      {
-        id: 2,
-        title: "The Big Bang Theory",
-        description: "Season 2",
-        image: "https://i.imgur.com/3n9xOxt.jpg",
-        kind: "series",
-      },
-      {
-        id: 3,
-        title: "The Big Bang Theory",
-        description: "Season 3",
-        image: "https://i.imgur.com/3n9xOxt.jpg",
-        kind: "series",
-      },
-      {
-        id: 4,
-        title: "The Big Bang Theory",
-        description: "Season 4",
-        image: "https://i.imgur.com/3n9xOxt.jpg",
-        kind: "series",
-      },
-      {
-        id: 5,
-        title: "The Big Bang Theory",
-        description: "Season 5",
-        image: "https://i.imgur.com/3n9xOxt.jpg",
-        kind: "series",
-      },
-      {
-        id: 6,
-        title: "The Big Bang Theory",
-        description: "Season 6",
-        image: "https://i.imgur.com/3n9xOxt.jpg",
-        kind: "series",
-      },
-      {
-        id: 7,
-        title: "The Big Bang Theory",
-        description: "Season 7",
-        image: "https://i.imgur.com/3n9xOxt.jpg",
-        kind: "series",
-      },
-      {
-        id: 8,
-        title: "The Big Bang Theory",
-        description: "Season 8",
-        image: "https://i.imgur.com/3n9xOxt.jpg",
-        kind: "series",
-      },
-      {
-        id: 9,
-        title: "The Big Bang Theory",
-        description: "Season 9",
-        image: "https://i.imgur.com/3n9xOxt.jpg",
-        kind: "series",
-      },
-      {
-        id: 10,
-        title: "The Big Bang Theory",
-        description: "Season 10",
-        image: "https://i.imgur.com/3n9xOxt.jpg",
-        kind: "series",
-      },
-      {
-        id: 11,
-        title: "A bela e a fera",
-        description: "",
-        image: "https://i.imgur.com/3n9xOxt.jpg",
-        kind: "movie",
-      },
-    ]);
+    fetchMedias()
+      .then((medias) => {
+        setMedias(medias);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
   });
 
   return (
     <Container>
-      <Form className="my-4">
-        <Form.Group controlId="search">
-          <Form.Control
-            type="search"
-            placeholder="Search movies and series..."
-          />
-        </Form.Group>
-      </Form>
-      <Row>
-        {medias.map((media) => (
-          <Col sm={12} md={6} lg={4} key={media.id}>
-            <Card className="mb-4">
-              <Card.Img variant="top" src={media.image} />
-              <Card.Body>
-                <Card.Title>{media.title}</Card.Title>
-                <Card.Text>{media.description}</Card.Text>
-                <Link to={`/details/${media.id}`}>
-                  <Button variant="primary">Watch</Button>
-                </Link>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      ) : (
+        <div>
+          {error && <div>{error}</div>}
+          {!error && (
+            <div>
+              <Form className="my-4">
+                <Form.Group controlId="search">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search movies and series..."
+                  />
+                </Form.Group>
+              </Form>
+              <Row>
+                {medias.map((media) => (
+                  <Col sm={12} md={6} lg={4} key={media.id}>
+                    <Card className="mb-4">
+                      {media.image && (
+                        <Card.Img variant="top" src={media.image} />
+                      )}
+                      <Card.Body>
+                        <Card.Title>{media.title}</Card.Title>
+                        <Card.Text>{media.description}</Card.Text>
+                        <Link to={`/details/${media.id}`}>
+                          <Button variant="primary">Watch</Button>
+                        </Link>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </div>
+          )}
+        </div>
+      )}
     </Container>
   );
 }
